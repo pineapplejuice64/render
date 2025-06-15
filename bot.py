94292a3a1cf5620fba22c90eae87f453b2b826e3
pineapplejuice64 <jmkmwangi04@gmail.com>
2025-06-15 16:05:45 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,9 +17,12 @@ while True:
     chrome_options = Options()
     
    
-   # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")  # disable GPU hardware acceleration
+    chrome_options.add_argument("--remote-debugging-port=9222")  # enables DevTools listening on this port
+    chrome_options.add_argument("--disable-software-rasterizer")  
     chrome_options.binary_location = "/usr/bin/google-chrome"
 
     service = Service("/usr/bin/chromedriver")
