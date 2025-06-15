a911943417c78ce582d75489172b53643b9e8e32
pineapplejuice64 <jmkmwangi04@gmail.com>
2025-06-15 14:02:16 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 import time
 import os
 
 # Run every 20 minutes forever
 while True:
     chrome_options = Options()
+    chrome_options.binary_location = "/opt/chrome-linux64/chrome"
+   
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
 
-    driver = webdriver.Chrome(options=chrome_options)
+    service = Service("/usr/local/bin/chromedriver")
+
+driver = webdriver.Chrome(service=service, options=chrome_options)
 
     try:
         # Open initial page to set cookies
